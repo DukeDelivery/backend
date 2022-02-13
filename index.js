216cd7c1d4e9f0fcdf3300d205eb923d6751942a
Del Cudjoe <ervin.cudjoe@duke.edu>
2022-02-12 21:16:50 -0500

    hero

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send(process.env.MONGODB_URI);
+  res.send('server online');
 })
 app.post('/sms', async (req, res) => {
 
