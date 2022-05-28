fca13121f9b43a3c2e5e80b7bb7fe05595441d21
Del Cudjoe <ervin.cudjoe@duke.edu>
2022-05-28 16:12:50 -0400

    updated request url

--- util/util.js
+++ util/util.js
@@ -1,9 +1,19 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 const path = require('path');
 
 const fileAt = (file) => path.join(__dirname, file);
 
 const sendText = (number, message) => {
-  console.log('Text: ', message);
+  client.messages
+    .create({
+      body: message,
+      from: '+12183962061',
+      to: number,
+    })
+    .then(x => console.log(x));
 }
 const formatDateString = (string) => {
   return new Date(string).toLocaleString('en-US', {dateStyle: 'medium', timeStyle: 'short'});
