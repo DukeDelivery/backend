40c9ba029fdd8b7e686683ca4a46d0a03ecc6671
Del Cudjoe <ervin.cudjoe@duke.edu>
2022-05-28 16:22:54 -0400

    fixed twilio issue

--- util/util.js
+++ util/util.js
@@ -1,5 +1,5 @@
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 const path = require('path');
